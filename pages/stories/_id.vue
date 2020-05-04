cfe65726dde0064350b6e605e3a89fb3af78e350
Maria Kotliarova <itwassoclear@gmail.com>
2020-05-04 19:10:30 +0300

    first version of footer

--- pages/stories/_id.vue
+++ pages/stories/_id.vue
@@ -1,8 +1,16 @@
-<template> </template>
+<template>
+  <div>
+    <app-footer />
+  </div>
+</template>
 
 <script>
+import Footer from '@/components/Footer';
 export default {
   name: '_id',
+  components: {
+    'app-footer': Footer,
+  },
 };
 </script>
 
