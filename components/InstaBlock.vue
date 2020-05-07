a214f0181ae283ed4e4192dc9c377775259618d9
leka-ren <polafort@gmail.com>
2020-05-07 16:08:06 +0300

    insta-block

---
+++ components/InstaBlock.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="insta-story">
+        <div class="insta-story__info">
+                <h2 class="insta-story__title">Инстаграм</h2>
+                <p class="insta-story__prolog">Два раза в неделю мы просматриваем все посты по хештегу #этонелечится. Все истории, где нет нецензурных выражений и запрещенного контента попадают сюда. Следите за правильным написанием хештега, чтобы мы не пропустили вашу историю.</p>
+            </div>
+            <div class="insta-story__images">
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+                <div class="insta-story__image"></div>
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.insta-story {
+    margin: 0 auto;
+    display: flex;
+    max-width: 95%;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.insta-story__info {
+    max-width: 26%;
+    flex-direction: column;
+}
+
+.insta-story__title {
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-bottom: 10px;
+    text-decoration: underline;
+    font-family: "Inter", Arial, sans-serif;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 36px;
+}
+
+.insta-story__prolog {
+    font-family: "Inter", Arial, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 22px;
+    color: #666666;
+}
+
+.insta-story__images {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 30px;
+    max-width: 74%;
+}
+
+.insta-story__image {
+    background-color: #EDEDED;
+    height: 195px;
+    width: 195px;
+}
+</style>
---
+++ components/ui/Cover.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="cover">
+            <h2 class="cover__title">Проект Благотворительного Фонда<br>Константина Хабенского</h2>
+            <p class="cover__subtitle">#РАКЛЕЧИТСЯ</p>
+            <p class="cover__quote">Истории людей, победивших рак,<br>но не свои привычки</p>
+            <p class="cover__excerpt">Есть вещи, которые не лечатся.<br>
+                Особенности характера, страстные<br>
+                увлечения, привычки, ставшие частью<br>
+                нашего «я», фобии, которые мы приобрели в<br>
+                детстве. Но это точно не рак. Рак лечится.<br>
+                Лучшее доказательство — люди с их<br>
+                историями.</p>
+        </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped>
+    body {
+    margin: 0;
+    padding: 0;
+}
+
+.cover {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    background-color: #7a29a3;
+}
+
+.cover__title {
+    padding-top: 55px;
+    margin: auto 0;
+    text-align: center;
+    width: 100%;
+    color: #fff;
+    font-weight: 100;
+    letter-spacing: 2.5px;
+    font-size: 16px;
+    line-height: 24px;
+    font-family: 'Intro', Arial, sans-serif;
+}
+
+.cover__subtitle {
+    padding-top: 80px;
+    margin: auto 0;
+    text-align: center;
+    width: 100%;
+    color: #ffffff;
+    font-size: 90px;
+    font-weight: 600;
+    font-family: 'Intro', Arial, sans-serif;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+}
+
+.cover__quote {
+    padding-top: 30px;
+    margin: auto 0;
+    text-align: center;
+    align-self: center;
+    width: 100%;
+    color: #ffffff;
+    font-size: 26px;
+    line-height: 26px;
+    text-align: center;
+    font-family: 'Intro', Arial, sans-serif;
+}
+
+.cover__excerpt {
+    padding-top: 55px;
+    padding-bottom: 70px;
+    margin: auto 0;
+    text-align: center;
+    align-self: center;
+    width: 100%;
+    color: #ffffff;
+    line-height: 26px;
+    text-align: center;
+    font-family: 'Intro', Arial, sans-serif;
+}
+</style>
