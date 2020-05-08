d983efc5fdc64078cf9b5c6d36f14857e72d5df3
Polina Ren <56325631+leka-ren@users.noreply.github.com>
2020-05-08 18:27:35 +0300

    Update Statistics.vue

--- components/Statistics.vue
+++ components/Statistics.vue
@@ -8,9 +8,7 @@
           000 человек.
         </p>
         <div class="statistics__info">
-          <div class="statistics__bar full-staus-bar">
-            <div class="statistics__bar-value full-staus-bar"></div>
-          </div>
+            <app-statusBar :value="1" :maxValue="3"/>
           <p class="statistics__total">1 из 3</p>
           <p class="statistics__who">Левада-Центр 2018</p>
         </div>
@@ -18,9 +16,7 @@
       <div class="statistics__block">
         <p class="statistics__quote">2,6% Россиян имеют онкозаболевания.</p>
         <div class="statistics__info">
-          <div class="statistics__bar full-staus-bar">
-            <div class="statistics__bar-value full-staus-bar"></div>
-          </div>
+          <app-statusBar :value="3" :maxValue="100" />
           <p class="statistics__total">3 700 000</p>
           <p class="statistics__who">Росстат 2018</p>
         </div>
@@ -30,8 +26,8 @@
           На 28% выросла доля выявления заболеваний на ранней стадии за 10 лет.
         </p>
         <div class="statistics__info">
-          <div class="statistics__bar half-staus-bar"></div>
-          <div class="statistics__bar-value half-staus-bar"></div>
+          <app-doubleBarWhite :value="35" :maxValue="100"/>
+          <app-doubleBarViolet :value="15" :maxValue="100"/>
           <p class="statistics__total">&#8593;28%</p>
           <p class="statistics__who">МНИОИ Герцена 2018</p>
         </div>
@@ -42,8 +38,8 @@
           диагноза.
         </p>
         <div class="statistics__info">
-          <div class="statistics__bar half-staus-bar"></div>
-          <div class="statistics__bar-value half-staus-bar"></div>
+          <app-doubleBarWhite :value="15" :maxValue="100"/>
+          <app-doubleBarViolet :value="35" :maxValue="100"/>
           <p class="statistics__total">&#8595;25%</p>
           <p class="statistics__who">МНИОИ Герцена 2018</p>
         </div>
@@ -53,18 +49,28 @@
 </template>
 
 <script>
-export default {};
+import statusBar from '@/components/ui/statusBar';
+import doubleBarWhite from '@/components/ui/doubleBarWhite';
+import doubleBarViolet from '@/components/ui/doubleBarViolet';
+export default {
+    components: {
+        'app-statusBar': statusBar,
+        'app-doubleBarWhite': doubleBarWhite,
+        'app-doubleBarViolet': doubleBarViolet,
+    }
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 .statistics {
   margin: 0 auto;
-  max-width: 95%;
+  max-width: 1440px;
+  width: 95%;
   display: flex;
   flex-direction: column;
 }
-
 .statistics__title {
+    text-align: left;
   max-width: 415px;
   padding-bottom: 38px;
   font-family: 'Inter', Arial, sans-serif;
@@ -73,12 +79,12 @@ export default {};
   font-size: 32px;
   line-height: 36px;
 }
-
 .statistics__blocks {
+    margin: 0 auto;
+    width: 100%;
   display: flex;
   justify-content: space-between;
 }
-
 .statistics__block {
   border: 1px solid #efefef;
   box-sizing: border-box;
@@ -88,8 +94,8 @@ export default {};
   flex-direction: column;
   justify-content: space-between;
 }
-
 .statistics__quote {
+    text-align: left;
   padding-top: 7px;
   padding-left: 20px;
   max-width: 260px;
@@ -99,24 +105,13 @@ export default {};
   font-size: 12px;
   line-height: 16px;
 }
-
 .statistics__info {
+  padding-bottom: 20px;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
   width: 88%;
 }
-
-.statistics__bar {
-  background-color: #e4e1e1;
-  max-width: 100%;
-}
-
-.statistics__bar-value {
-  background: #613a93;
-  width: 25%;
-}
-
 .statistics__total {
   margin: 0;
   padding-top: 20px;
@@ -127,8 +122,8 @@ export default {};
   line-height: 40px;
   text-align: right;
 }
-
 .statistics__who {
+  padding-top: 15px;
   font-family: 'Inter', Arial, sans-serif;
   font-style: normal;
   font-weight: normal;
@@ -138,11 +133,10 @@ export default {};
   color: #666666;
 }
 
-.full-staus-bar {
-  height: 40px;
-}
-
-.half-staus-bar {
-  height: 20px;
+@media screen and (max-width: 1350px) {
+    .statistics__block {
+        width: 265px;
+        height: 265px;
+    }
 }
 </style>
