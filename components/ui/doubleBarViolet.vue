f1e6431a780ff572b3c76b1a2f17d8ff749ab5b9
Polina Ren <56325631+leka-ren@users.noreply.github.com>
2020-05-08 18:29:22 +0300

    Create doubleBarViolet.vue

---
+++ components/ui/doubleBarViolet.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="statistics__bar-value half-staus-bar" :style="style"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'fullBar',
+        props: {
+            value: Number,
+            maxValue: Number
+        },
+        computed: {
+            style() {
+                return `width: ${this.value / this.maxValue * 100}%`
+            }
+        }
+    }
+</script>
+
+<style scoped>
+.statistics__bar-value {
+  background: #613a93;
+}
+.half-staus-bar {
+  height: 20px;
+}
+</style>
