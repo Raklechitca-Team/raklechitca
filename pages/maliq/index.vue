379ec26a89ce9ab5e807c7feb825abf8b946dee9
Malik Batyrov <batyrov.m.m@yandex.ru>
2020-05-10 14:49:54 +0300

    change button

--- pages/maliq/index.vue
+++ pages/maliq/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <app-header></app-header>
-    <bt-read-more></bt-read-more>
     <cover-qm></cover-qm>
+    <bt-fill-form></bt-fill-form>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header';
-import ButtonReadMore from '../../components/ui/ButtonReadMore.vue';
 import CoverSizeSmall from '../../components/CoverSizeSmall.vue';
+import ButtonFillFormVue from '../../components/ui/ButtonFillForm.vue';
 export default {
   name: 'stories',
   components: {
     'app-header': Header,
-    'bt-read-more': ButtonReadMore,
     'cover-qm': CoverSizeSmall,
+    'bt-fill-form': ButtonFillFormVue,
   },
 };
 </script>
