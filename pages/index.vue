64ca4078c2ac0840ceb706a9f528b0572c62c89a
Egor Shkirya <zuhijan@yandex.ru>
2020-05-13 11:42:49 +0600

    fix cardlist index page error, and fix vuex

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <app-title />
     <app-video />
     <app-cover-in-site :text="'И В ОТЛИЧИЕ ОТ РАКА,'" />
-    <!--    <app-card-list  :cards="stories" />-->
+    <app-cardlist :cards="stories" />
     <app-cover-in-site :text="`РАССКАЗЫВАЙТЕ ВАШИ ИСТОРИИ В ИНСТАГРАМ`" />
     <app-insta />
     <app-tell-story />
@@ -20,23 +20,26 @@ import TellStory from '@/components/TellStory';
 import Statistics from '@/components/Statistics';
 import Cover from '@/components/Cover';
 import CoverInSite from '@/components/CoverInSite';
-// import CardList from '@/components/CardList/CardList';
+import CardList from '@/components/Cardlist/CardList';
 export default {
   name: 'index',
   components: {
     'app-title': Title,
     'app-video': Video,
     'app-cover-in-site': CoverInSite,
-    // 'app-card-list': CardList,
+    'app-cardlist': CardList,
     'app-insta': InstaBlock,
     'app-tell-story': TellStory,
     'app-statistics': Statistics,
     'app-cover': Cover,
   },
+  data() {
+    return {};
+  },
   computed: {
-    // stories() {
-    // return this.$store.getters.stories.stories;
-    // },
+    stories() {
+      return this.$store.getters['stories/stories'];
+    },
   },
 };
 </script>
