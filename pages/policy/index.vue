55ca5b7b577a542b7bdb0d5522b1db833baae907
Malik Batyrov <batyrov.m.m@yandex.ru>
2020-05-14 21:17:56 +0300

    Политика обработки данных

---
+++ pages/policy/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <app-privacy />
+  </div>
+</template>
+
+<script>
+import PrivacyPolicy from '@/components/PrivacyPolicy';
+export default {
+  name: 'index',
+  components: {
+    'app-privacy': PrivacyPolicy,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  font-family: 'Inter', 'Arial', sans-serif;
+}
+</style>
