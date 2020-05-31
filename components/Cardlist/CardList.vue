29ef0707aaeadca2f966ed047c9110c9b023ac74
Maria Kotliarova <itwassoclear@gmail.com>
2020-05-31 21:44:50 +0300

    for fixed cardlist styles

--- components/Cardlist/CardList.vue
+++ components/Cardlist/CardList.vue
@@ -3,7 +3,7 @@
     <app-card
       v-for="card in cards"
       :key="card.id"
-      :url="`https://strapi.kruzhok.io${card.ImageUrl[0].url}`"
+      :url="`${baseUrl}${card.ImageUrl[0].url}`"
       :name="card.author"
       :text="card.title"
       :card="card.id"
@@ -19,6 +19,11 @@ export default {
   },
   props: {
     cards: Array,
+  },
+  computed: {
+    baseUrl() {
+      return process.env.baseUrl;
+    },
   },
 };
 </script>
